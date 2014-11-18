268c67d6848c9c0f9836f89c11782286bb66249a
Qi <zhan2032@purdue.edu>
2014-11-18 11:02:37 -0500

    changed

--- calc.py
+++ calc.py
@@ -75,13 +75,10 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-
         elif (op == "%"):
             print "Remainder: ", mod(a, b)
-
 		elif (op == "**"):
 			print "Exponent: ", exp(a, b)
-
         else:
             print "Invalid operation..."
 
